84a74d3a0c00169c852d593e1529b99d87b04320
noordaryahya <noor.daryahya@mail.huji.ac.il>
2024-07-09 10:13:11 +0300

    schema

--- models/user-moudels.js
+++ models/user-moudels.js
@@ -4,8 +4,10 @@ mongoose.connect('mongodb://localhost:27017/mydb');
 
 
 var personalSchema = mongoose.model({
-
-})
+    name:{type:String ,required:true},
+    age: Number,
+    nationality:String,
+});
 var Person = mongoose.model("Person",personalSchema);
 module.exports = Person;
 
